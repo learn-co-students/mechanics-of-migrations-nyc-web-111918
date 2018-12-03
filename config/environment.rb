0106fe3abc23e72198c5af0e38663b0415033fc1
Tonyjlum <tonyjlum@gmail.com>
2018-12-02 23:58:21 -0500

    start database

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,18 @@ Bundler.require
 
 
 # put the code to connect to the database here
+# DB = {:conn => SQLite3::Database.new("db/artists.db")}
+
+DB = ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :database => "../db/artists"
+    )
+
+  DB = ActiveRecord::Base.connection
+
+  if ENV["ACTIVE_RECORD_ENV"] == "test"
+    ActiveRecord::Migration.verbose = false
+  end
 
 
 require_relative "../artist.rb"
